4eefd76ec43052bd79cb82ea0bdcdd319e14f5a1
kongqiuqiang <kongqiuqiang@bytedance.com>
2021-09-16 16:53:43 +0800

    update

--- separate_scripts/download_checkpoints.sh
+++ separate_scripts/download_checkpoints.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-ZENODO_PATH="https://zenodo.org/record/5507029/files"
+ZENODO_DIR="https://zenodo.org/record/5507029/files"
 DOWNLOADED_CHECKPOINT_DIR="./downloaded_checkpoints"
 
 mkdir -p $DOWNLOADED_CHECKPOINT_DIR
 
 MODEL_NAME="resunet143_ismir2021_vocals_8.9dB_350k_steps.pth"
-wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_PATH}/${MODEL_NAME}?download=1"
+wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_DIR}/${MODEL_NAME}?download=1"
 
 MODEL_NAME="resunet143_ismir2021_accompaniment_16.8dB_350k_steps.pth"
-wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_PATH}/${MODEL_NAME}?download=1"
+wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_DIR}/${MODEL_NAME}?download=1"
+
+MODEL_NAME="resunet143_subbtandtime_vocals_8.8dB_350k_steps.pth"
+wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_DIR}/${MODEL_NAME}?download=1"
+
+MODEL_NAME="resunet143_subbtandtime_accompaniment_16.4dB_350k_steps.pth"
+wget -O "${DOWNLOADED_CHECKPOINT_DIR}/${MODEL_NAME}" "${ZENODO_DIR}/${MODEL_NAME}?download=1"
