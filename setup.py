0a088e1fc852a15d7e558a7e203888de0577dfb1
QiuqiangKong <qiuqiangkong@gmail.com>
2021-10-22 00:20:25 +0800

    0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bytesep',
-    version='0.0.3',
+    version='0.0.4',
     description='Music source separation',
     author='ByteDance',
     url="https://github.com/bytedance/music_source_separation",
